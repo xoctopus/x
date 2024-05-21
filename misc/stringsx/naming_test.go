eb5985475e3898c698e13a48c1c2a3c068b4778b
sincos <birdyfj@gmail.com>
2024-05-21 19:12:24 +0800

    feat(misc): strings helper

---
+++ misc/stringsx/naming_test.go
@@ -0,0 +1,22 @@
+package stringsx_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	. "github.com/sincospro/x/misc/stringsx"
+)
+
+func TestNaming(t *testing.T) {
+	name := "i_am_a_10_years_senior"
+
+	NewWithT(t).Expect(LowerCamelCase(name)).To(Equal("iAmA10YearsSenior"))
+	NewWithT(t).Expect(LowerSnakeCase(name)).To(Equal("i_am_a_10_years_senior"))
+	NewWithT(t).Expect(UpperCamelCase(name)).To(Equal("IAmA10YearsSenior"))
+	NewWithT(t).Expect(UpperSnakeCase(name)).To(Equal("I_AM_A_10_YEARS_SENIOR"))
+	NewWithT(t).Expect(UpperSnakeCase(name)).To(Equal("I_AM_A_10_YEARS_SENIOR"))
+	NewWithT(t).Expect(LowerDashJoint(name)).To(Equal("i-am-a-10-years-senior"))
+
+	NewWithT(t).Expect(UpperCamelCase("OrgID")).To(Equal("OrgID"))
+}
---
+++ misc/stringsx/split_test.go
@@ -0,0 +1,37 @@
+package stringsx_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/sincospro/x/misc/stringsx"
+)
+
+func Test_SplitToWords(t *testing.T) {
+	words := []string{"I", "Am", "A", "10", "Years", "Senior"}
+	cases := []struct {
+		phrase string
+		words  []string
+	}{
+		{"IAmA10YearsSenior", words},
+		{"I Am A 10 Years Senior", words},
+		{". I_ Am_A_10_Years____Senior__", words},
+		{"I-~~ Am\nA\t10 Years *** Senior", words},
+		{"lowercase", []string{"lowercase"}},
+		{"Class", []string{"Class"}},
+		{"MyClass", []string{"My", "Class"}},
+		{"HTML", []string{"HTML"}},
+		{"QOSType", []string{"QOS", "Type"}},
+		{"\xF1\xF4\x8F\xBF\xBF", []string{"\xF1\xF4\x8F\xBF\xBF"}},
+	}
+	for _, c := range cases {
+		NewWithT(t).Expect(stringsx.SplitToWords(c.phrase)).To(Equal(c.words))
+	}
+
+	t.Log(stringsx.CheckLetterType('1'))
+	t.Log(stringsx.CheckLetterType('a'))
+	t.Log(stringsx.CheckLetterType('A'))
+	t.Log(stringsx.CheckLetterType('+'))
+	t.Log(stringsx.CheckLetterType('🚀'))
+}
