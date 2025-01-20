9e68fcd48c3ac7462e8751bd9065cbbef7b7602e
saito <birdyfj@gmail.com>
2025-01-20 11:58:34 +0800

    test: add tests for fuctions with multi type parameters

--- typex/testdata/function.go
+++ typex/testdata/function.go
@@ -6,16 +6,20 @@ import (
 )
 
 type (
-	Func              func(x, y string, z ...int) (Boolean, error)
-	Curry             func(x String, y ...fmt.Stringer) func() string
-	Max[T comparable] func(...T) T
+	Func                           func(x, y string, z ...int) (Boolean, error)
+	Curry                          func(x String, y ...fmt.Stringer) func() string
+	Max[T comparable]              func(...T) T
+	Contains[S ~[]E, E comparable] func(s S, v E) bool
+	ContainsFunc[S ~[]E, E any]    func(s S, f func(E) bool) bool
 )
 
 type Functions struct {
-	Func  Func
-	Curry Curry
-	Uname func() func() func() string
-	Max   Max[int]
+	Func            Func
+	Curry           Curry
+	Uname           func() func() func() string
+	Max             Max[int]
+	ContainsInt     Contains[[]int, int]
+	ContainsIntFunc ContainsFunc[[]int, int]
 }
 
 var (
