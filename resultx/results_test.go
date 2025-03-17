02526713108f79795dce7da85fa14815bafcf2d9
saito <birdyfj@gmail.com>
2025-03-17 18:21:08 +0800

    test(resultx): add unit test

--- resultx/results_test.go
+++ resultx/results_test.go
@@ -13,6 +13,9 @@ func f() (int, bool, string) { return 100, true, "f" }
 func TestAt(t *testing.T) {
 	rs := resultx.ResultsOf(f())
 	NewWithT(t).Expect(resultx.At[int](rs, 0)).To(Equal(100))
+	NewWithT(t).Expect(rs.At(0)).To(Equal(100))
 	NewWithT(t).Expect(resultx.At[bool](rs, 1)).To(BeTrue())
+	NewWithT(t).Expect(rs.At(1)).To(BeTrue())
 	NewWithT(t).Expect(resultx.At[string](rs, 2)).To(Equal("f"))
+	NewWithT(t).Expect(rs.At(2)).To(Equal("f"))
 }
