a5b9f79d8d269c341211046efbba2a37c7800598
saito <birdyfj@gmail.com>
2025-10-16 21:56:04 +0900

    feat(syncx): typed sync.map

---
+++ syncx/map.go
@@ -0,0 +1,198 @@
+package syncx
+
+import (
+	"sync"
+
+	"github.com/xoctopus/x/mapx"
+)
+
+type Map[K comparable, V any] interface {
+	Store(K, V)
+
+	Clear()
+	Delete(K)
+	CompareAndDelete(key K, old V) (deleted bool)
+
+	Swap(k K, new V) (previous V, loaded bool)
+	CompareAndSwap(key K, old, new V) (swapped bool)
+
+	Load(K) (V, bool)
+	LoadAndDelete(K) (v V, loaded bool)
+	LoadOrStore(K, V) (actual V, loaded bool)
+	Range(func(K, V) bool)
+}
+
+func NewXmap[K comparable, V any]() Map[K, V] {
+	return &xmap[K, V]{
+		mtx: &sync.RWMutex{},
+		m:   make(map[K]V),
+	}
+}
+
+func AsXmap[K comparable, V any, M ~map[K]V](m M) Map[K, V] {
+	return &xmap[K, V]{
+		mtx: &sync.RWMutex{},
+		m:   m,
+	}
+}
+
+type xmap[K comparable, V any] struct {
+	mtx *sync.RWMutex
+	m   map[K]V
+}
+
+func (m *xmap[K, V]) Store(k K, v V) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	m.m[k] = v
+}
+
+func (m *xmap[K, V]) Clear() {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	m.m = make(map[K]V)
+}
+
+func (m *xmap[K, V]) Delete(k K) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	delete(m.m, k)
+}
+
+func (m *xmap[K, V]) CompareAndDelete(k K, old V) (deleted bool) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	if x, ok := m.m[k]; ok && any(x) == any(old) {
+		delete(m.m, k)
+		return true
+	}
+	return false
+}
+
+func (m *xmap[K, V]) Swap(k K, new V) (previous V, loaded bool) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	previous, loaded = m.m[k]
+	m.m[k] = new
+	return previous, loaded
+}
+
+func (m *xmap[K, V]) CompareAndSwap(k K, old, new V) (swapped bool) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	if x, ok := m.m[k]; ok && any(x) == any(old) {
+		m.m[k] = new
+		return true
+	}
+	return false
+}
+
+func (m *xmap[K, V]) Load(k K) (v V, loaded bool) {
+	m.mtx.RLock()
+	defer m.mtx.RUnlock()
+	v, loaded = m.m[k]
+	return
+}
+
+func (m *xmap[K, V]) LoadAndDelete(k K) (v V, loaded bool) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	v, loaded = m.m[k]
+	if loaded {
+		delete(m.m, k)
+	}
+	return
+}
+
+func (m *xmap[K, V]) LoadOrStore(k K, v V) (actual V, loaded bool) {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	actual, loaded = m.m[k]
+	if loaded {
+		return
+	}
+	m.m[k] = v
+	return v, loaded
+}
+
+func (m *xmap[K, V]) Range(f func(K, V) bool) {
+	m.mtx.RLock()
+	m.mtx.RUnlock()
+	for k, v := range m.m {
+		if !f(k, v) {
+			break
+		}
+	}
+}
+
+func NewSmap[K comparable, V any]() Map[K, V] {
+	return mapx.NewSmap[K, V]()
+}
+
+func AsSmap[K comparable, V any, M ~map[K]V](m M) Map[K, V] {
+	sm := &sync.Map{}
+	for k, v := range m {
+		sm.Store(k, v)
+	}
+	return &smap[K, V]{m: sm}
+}
+
+type smap[K comparable, V any] struct {
+	m *sync.Map
+}
+
+func (m *smap[K, V]) Store(k K, v V) {
+	m.m.Store(k, v)
+}
+
+func (m *smap[K, V]) Clear() {
+	m.m.Clear()
+}
+
+func (m *smap[K, V]) Delete(k K) {
+	m.m.Delete(k)
+}
+
+func (m *smap[K, V]) CompareAndDelete(k K, old V) (deleted bool) {
+	return m.m.CompareAndDelete(k, old)
+}
+
+func (m *smap[K, V]) Swap(k K, new V) (previous V, loaded bool) {
+	return m.result(m.m.Swap(k, new))
+}
+
+func (m *smap[K, V]) CompareAndSwap(k K, old, new V) (swapped bool) {
+	return m.m.CompareAndSwap(k, old, new)
+}
+
+func (m *smap[K, V]) Load(k K) (v V, loaded bool) {
+	return m.result(m.m.Load(k))
+}
+
+func (m *smap[K, V]) LoadAndDelete(k K) (v V, loaded bool) {
+	return m.result(m.m.LoadAndDelete(k))
+}
+
+func (m *smap[K, V]) LoadOrStore(k K, v V) (actual V, loaded bool) {
+	return m.result(m.m.LoadOrStore(k, v))
+}
+
+func (m *smap[K, V]) Range(f func(K, V) bool) {
+	m.m.Range(func(key, value any) bool { return f(key.(K), value.(V)) })
+}
+
+func (m *smap[K, V]) result(x any, b bool) (V, bool) {
+	if v, ok := x.(V); ok {
+		return v, b
+	}
+	return *new(V), b
+}
+
+// type Set[K comparable, V any] struct {
+// 	Map[K, struct{}]
+// }
---
+++ syncx/map_test.go
@@ -0,0 +1,133 @@
+package syncx_test
+
+import (
+	"testing"
+
+	"github.com/xoctopus/x/ptrx"
+	"github.com/xoctopus/x/syncx"
+	. "github.com/xoctopus/x/testx"
+)
+
+func results(v ...any) []any {
+	return v
+}
+
+func TestMap(t *testing.T) {
+	k1 := ptrx.Ptr(1)
+	k2 := ptrx.Ptr(2)
+	// k3 := ptrx.Ptr(3)
+	// equal := func(x any) func(k any) bool {
+	// 	return func(k any) bool {
+	// 		if k == x {
+	// 			return true
+	// 		}
+	// 		return reflect.DeepEqual(x, k)
+	// 	}
+	// }
+
+	Expect(t, k1, Equal(ptrx.Ptr(1)))
+	Expect(t, k1 == ptrx.Ptr(1), BeFalse())
+
+	for _, m := range []syncx.Map[any, any]{
+		syncx.NewSmap[any, any](),
+		syncx.NewXmap[any, any](),
+		syncx.AsSmap(map[any]any{}),
+		syncx.AsXmap(map[any]any{}),
+	} {
+		Expect(t, results(m.Load(k1)), Equal([]any{nil, false}))
+
+		m.Store(k1, 100)
+		Expect(t, results(m.Load(k1)), Equal([]any{100, true}))
+		Expect(t, results(m.Load(k2)), Equal([]any{nil, false}))
+
+		m.Delete(ptrx.Ptr(1))
+		Expect(t, results(m.Load(k1)), Equal([]any{100, true}))
+		Expect(t, results(m.Load(k2)), Equal([]any{nil, false}))
+
+		Expect(t, m.CompareAndSwap(k1, 101, 102), BeFalse())
+		Expect(t, m.CompareAndSwap(k1, 100, 102), BeTrue())
+
+		Expect(t, m.CompareAndDelete(k1, 100), BeFalse())
+		Expect(t, m.CompareAndDelete(k1, 102), BeTrue())
+
+		m.Store(k1, 100)
+		Expect(t, results(m.Swap(k1, 102)), Equal([]any{100, true}))
+
+		m.Delete(k1)
+		Expect(t, results(m.Load(k1)), Equal([]any{nil, false}))
+
+		m.Store(k1, 100)
+		Expect(t, results(m.LoadOrStore(k1, 101)), Equal([]any{100, true}))
+		Expect(t, results(m.LoadOrStore(k2, 101)), Equal([]any{101, false}))
+
+		count := 0
+		counter := func(k any, v any) bool {
+			count++
+			return true
+		}
+		m.Range(counter)
+		Expect(t, count, Equal(2))
+
+		Expect(t, results(m.LoadAndDelete(k1)), Equal([]any{100, true}))
+
+		count = 0
+		m.Range(counter)
+		Expect(t, count, Equal(1))
+
+		m.Clear()
+		count = 0
+		m.Range(counter)
+		Expect(t, count, Equal(0))
+	}
+
+	m := syncx.AsSmap(map[any]any{k1: 100})
+	Expect(t, results(m.Load(k1)), Equal([]any{100, true}))
+
+	m = syncx.AsXmap(map[any]any{k1: 100})
+	m.Range(func(k any, v any) bool {
+		if v == 100 {
+			x, ok := k.(*int)
+			Expect(t, ok, BeTrue())
+			Expect(t, x, Equal(k1))
+			return false
+		}
+		return true
+	})
+}
+
+// func TestSet(t *testing.T) {
+// 	for _, set := range []mapx.Set[int]{mapx.NewSet[int](), mapx.NewSafeSet[int]()} {
+// 		set.Store(1, 2, 3)
+// 		NewWithT(t).Expect(set.Exists(1)).To(BeTrue())
+// 		set.Delete(1)
+// 		NewWithT(t).Expect(set.Exists(1)).To(BeFalse())
+// 		NewWithT(t).Expect(set.Keys()).To(ConsistOf(2, 3))
+// 		NewWithT(t).Expect(set.Len()).To(Equal(2))
+//
+// 		f := func(expect int, has *bool) func(k int) bool {
+// 			return func(k int) bool {
+// 				if k == expect {
+// 					*has = true
+// 					return false
+// 				}
+// 				return true
+// 			}
+// 		}
+//
+// 		has := false
+// 		expect := 2
+// 		set.Range(f(expect, &has))
+// 		NewWithT(t).Expect(has).To(BeTrue())
+//
+// 		has = false
+// 		expect = 1
+// 		set.Range(f(expect, &has))
+// 		NewWithT(t).Expect(has).To(BeFalse())
+//
+// 		set2 := set.Clone()
+// 		NewWithT(t).Expect(set.Equal(set2)).To(BeTrue())
+//
+// 		set.Clear()
+// 		NewWithT(t).Expect(set.Len()).To(Equal(0))
+// 	}
+// }
